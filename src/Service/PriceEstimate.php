95db60de647551da7dce525eeaed6a7156ac6628
kevin muziak <lepolak01@gmail.com>
2023-12-16 10:45:07 +0100

    modifs readme

--- src/Service/PriceEstimate.php
+++ src/Service/PriceEstimate.php
@@ -5,8 +5,8 @@ namespace App\Service;
 class PriceEstimate 
 {
     public function EstimatePriceLocation (int $year, int $nbkm, int $nbdays): int {
-        $totalPrice = $year*0.5+$nbkm*0.6+$nbdays*30;
-        return $totalPrice;
+        $totalPriceEstimate = (($year*0.5)/1000)*$nbkm*0.6+$nbdays*30;
+        return $totalPriceEstimate;
     }
 
 
